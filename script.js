deac56b683dda92842236617366c96ca4c86d65e
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-09-28 04:52:07 +0000

    Add javascript to manipulate DOM
    -Add text to display score of both Computer and
     Player
    -Add event listeners to the buttons
     (userchoices,playagainbutton)
    -Change Score and Display round result when both
     scores<5(when userchoices is clicked)
    -Display final result and Add playagainbutton when
     either of scores===5(when userchoices is clicked)
    -Remove textContents of score,roundresult,
     finalresult(when playagainbutton is clicked)

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function getComputerChoice() {
     else
         return "scissors";
 }
-function getHumanChoice() {
-    return prompt("Enter any of options(rock or paper or scissors)");
-}
+
 let humanScore = 0;
 let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
@@ -18,14 +16,16 @@ function playRound(humanChoice, computerChoice) {
         if (humanChoice === "paper") {
             console.log("You win!Paper beats Rock");
             humanScore++;
+            roundresult.textContent = "You win!Paper beats Rock";
         }
         else if (humanChoice === "scissors") {
             console.log("You Lose!Rock beats Scissors");
             computerScore++;
+            roundresult.textContent = "You Lose!Rock beats Scissors";
         }
         else {
             console.log("It's a tie,Play Again");
-            return playRound(getHumanChoice(), getComputerChoice());
+            roundresult.textContent = "It's a tie,Play Again";
         }
 
     }
@@ -33,14 +33,16 @@ function playRound(humanChoice, computerChoice) {
         if (humanChoice === "scissors") {
             console.log("You win!Scissors beat Paper");
             humanScore++;
+            roundresult.textContent = "You win!Scissors beat Paper";
         }
         else if (humanChoice === "rock") {
             console.log("You Lose!Paper beats Rock");
             computerScore++;
+            roundresult.textContent = "You Lose!Paper beats Rock";
         }
         else {
             console.log("It's a tie,Play Again");
-            return playRound(getHumanChoice(), getComputerChoice());
+            roundresult.textContent = "It's a tie,Play Again";
 
         }
     }
@@ -48,14 +50,67 @@ function playRound(humanChoice, computerChoice) {
         if (humanChoice === "rock") {
             console.log("You win!Rock beats Scissors");
             humanScore++;
+            roundresult.textContent = "You win!Rock beats Scissors";
         }
         else if (humanChoice === "paper") {
             console.log("You Lose!Scissors beat Paper");
             computerScore++;
+            roundresult.textContent = "You Lose!Scissors beat Paper";
         }
         else {
             console.log("It's a tie,Play Again");
-            return playRound(getHumanChoice(), getComputerChoice());
+            roundresult.textContent = "It's a tie,Play Again";
         }
     }
 }
+let userChoice = document.querySelector(".userchoices");
+let result = document.querySelector(".result");
+let compresult = document.querySelector(".result .compScoreMessage");
+let humresult = document.querySelector(".result .humScoreMessage");
+let finalResult = document.createElement("div");
+let roundresult = document.querySelector(".roundresult");
+let playAgain = document.createElement("button");
+playAgain.textContent = 'Play Again';
+playAgain.classList.add("playagainbutton");
+
+let computerScoreText = document.createElement("span");
+computerScoreText.textContent = "Computer Score: ";
+computerScoreText.style.color = "red";
+let compScoreMessage = document.createElement("span");
+compresult.appendChild(computerScoreText);
+compresult.appendChild(compScoreMessage);
+
+let humanScoreText = document.createElement("span");
+humanScoreText.textContent = "Your Score: ";
+humanScoreText.style.color = "blue";
+let humScoreMessage = document.createElement("span");
+humresult.appendChild(humanScoreText);
+humresult.appendChild(humScoreMessage);
+
+userChoice.addEventListener("click", event => {
+    if (humanScore < 5 && computerScore < 5) {
+        playRound(event.target.id, getComputerChoice());
+        compScoreMessage.textContent = `${computerScore}`;
+        humScoreMessage.textContent = `${humanScore}`;
+    }
+   if(computerScore==5||humanScore==5){
+
+        if (computerScore >= 5) {
+            finalResult.textContent = 'You lost Dude no worries you can try again';
+        }
+        else {
+            finalResult.textContent = 'Hey you won!Wanna Play Again?';
+        }
+        result.appendChild(finalResult);
+        result.appendChild(playAgain);
+    }
+});
+playAgain.addEventListener('click', (e) => {
+            humanScore = 0;
+            computerScore = 0;
+            compScoreMessage.textContent = ``;
+            humScoreMessage.textContent = ``;
+            finalResult.textContent = '';
+            roundresult.textContent='';
+            e.target.parentNode.removeChild(e.target);;
+        });
